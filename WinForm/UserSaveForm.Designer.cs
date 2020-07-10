88e0667a1b89752d15224ddc5b1d6082aa40eb0b
LibertyReman <libertyreman@gmail.com>
2020-07-11 01:00:20 +0900

    ラジオボックスを表示

--- WinForm/UserSaveForm.Designer.cs
+++ WinForm/UserSaveForm.Designer.cs
@@ -33,6 +33,11 @@
             this.MailCheckBox = new System.Windows.Forms.CheckBox();
             this.MailAddressTextBox = new System.Windows.Forms.TextBox();
             this.MailAddressLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.FreeRadioButton = new System.Windows.Forms.RadioButton();
+            this.BusinessRadioButton = new System.Windows.Forms.RadioButton();
+            this.NoteLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // IdLabel
@@ -79,11 +84,55 @@
             this.MailAddressLabel.TabIndex = 3;
             this.MailAddressLabel.Text = "メールアドレス";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BusinessRadioButton);
+            this.groupBox1.Controls.Add(this.FreeRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(24, 114);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 68);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "プラン";
+            // 
+            // FreeRadioButton
+            // 
+            this.FreeRadioButton.AutoSize = true;
+            this.FreeRadioButton.Location = new System.Drawing.Point(7, 29);
+            this.FreeRadioButton.Name = "FreeRadioButton";
+            this.FreeRadioButton.Size = new System.Drawing.Size(47, 16);
+            this.FreeRadioButton.TabIndex = 0;
+            this.FreeRadioButton.TabStop = true;
+            this.FreeRadioButton.Text = "無料";
+            this.FreeRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // BusinessRadioButton
+            // 
+            this.BusinessRadioButton.AutoSize = true;
+            this.BusinessRadioButton.Location = new System.Drawing.Point(101, 29);
+            this.BusinessRadioButton.Name = "BusinessRadioButton";
+            this.BusinessRadioButton.Size = new System.Drawing.Size(60, 16);
+            this.BusinessRadioButton.TabIndex = 1;
+            this.BusinessRadioButton.TabStop = true;
+            this.BusinessRadioButton.Text = "ビジネス";
+            this.BusinessRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // NoteLabel
+            // 
+            this.NoteLabel.AutoSize = true;
+            this.NoteLabel.Location = new System.Drawing.Point(31, 189);
+            this.NoteLabel.Name = "NoteLabel";
+            this.NoteLabel.Size = new System.Drawing.Size(91, 12);
+            this.NoteLabel.TabIndex = 6;
+            this.NoteLabel.Text = "無制限で使えます";
+            // 
             // UserSaveForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(501, 366);
+            this.Controls.Add(this.NoteLabel);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.MailAddressTextBox);
             this.Controls.Add(this.MailAddressLabel);
             this.Controls.Add(this.MailCheckBox);
@@ -92,6 +141,8 @@
             this.Name = "UserSaveForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ユーザ登録";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,6 +155,10 @@
         private System.Windows.Forms.CheckBox MailCheckBox;
         private System.Windows.Forms.TextBox MailAddressTextBox;
         private System.Windows.Forms.Label MailAddressLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton BusinessRadioButton;
+        private System.Windows.Forms.RadioButton FreeRadioButton;
+        private System.Windows.Forms.Label NoteLabel;
     }
 }
 
