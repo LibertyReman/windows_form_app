dbb54c2f5d77accd19e630c76d15f827ac98742b
LibertyReman <libertyreman@gmail.com>
2020-07-11 18:23:51 +0900

    Showで画面表示するように書き換え

--- Show_vs_ShowDialog/Show_vs_ShowDialog/Form1.cs
+++ Show_vs_ShowDialog/Show_vs_ShowDialog/Form1.cs
@@ -20,16 +20,8 @@ namespace Show_vs_ShowDialog
         private void AAAFormButton_Click(object sender, EventArgs e)
         {
             var f = new AAAForm();
-            //AAAFormはShowDialogで表示したのでダイアログリザルトを設定した場合、自動で画面が閉じる
-            f.ShowDialog();
-            if(f.DialogResult == DialogResult.OK)
-            {
-                MessageBox.Show("OKが押されました");
-            }
-            else if (f.DialogResult == DialogResult.Cancel)
-            {
-                MessageBox.Show("Chancelが押されました");
-            }
+            //AAAFormはShowで表示したのでダイアログリザルトを設定した場合でも、自動で画面が閉じない
+            f.Show();
         }
 
         private void BBBFormButton_Click(object sender, EventArgs e)
